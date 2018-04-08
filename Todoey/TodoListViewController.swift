5f40d1c21c763293a9bc59cf96601fa46c7851e4
Doug <androidsoong@gmail.com>
2018-04-08 10:09:17 -0500

    Added new item functionality using custom alert.

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -72,4 +72,25 @@ class TodoListViewController: UITableViewController
         return itemArray.count
     }
     
+    //MARK - Add New Items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem)
+    {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todoey Item",
+                                      message: "",
+                                      preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            self.itemArray.append(Item(value: textField.text!))
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        alert.addAction(action);
+        
+        present(alert, animated: true, completion: nil)
+    }
 }
